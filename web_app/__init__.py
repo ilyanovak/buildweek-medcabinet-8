b9bc24c489e13e9a4710f7344c869ca14e1942da
rubyclaguna <rubyclaguna@gmail.com>
2020-06-18 21:49:08 -0700

    Add nlp model to app

--- web_app/__init__.py
+++ web_app/__init__.py
@@ -2,11 +2,13 @@ from flask import Flask
 from web_app.routes.home_routes import home_routes
 from web_app.routes.json_routes import json_routes
 from web_app.routes.insert_routes import insert_routes
+from web_app.routes.search_routes import search
 
 app = Flask(__name__)
 
 app.register_blueprint(home_routes)
 app.register_blueprint(json_routes)
 app.register_blueprint(insert_routes)
+app.reguster_blueprint(search_routes)
 
 app.run(debug=True)
---
+++ web_app/models/nlp_model.py
@@ -0,0 +1,15 @@
+import pickle 
+import os 
+
+class Predictor(): 
+    def __init__(self):
+        # Load model 
+        self.nn = pickle.load(open(os.getcwd()+"/StrainAPI/models/knn_model.pkl", "rb"))
+        self.tfidf = pickle.load(open(os.getcwd()+"/StrainAPI/models/knn_tfidf.pkl", "rb"))
+
+    def predict(self,user_text,size=5): 
+        # Create Vector 
+        request = self.tfidf.transform([user_text])
+        # Use knn model to calculate the top n strains 
+        strain_ids = self.nn.kneighbors(request.todense(), n_neighbors=size)[1][0]
+        return strain_ids
